ada055916436ac52564872759f89930d79ca4e44
Andreas Høj <anho@universal-robots.com>
2024-12-16 23:03:06 +0100

    trigger update in the browser

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,5 +6,5 @@
         const el: HTMLButtonElement = "hello";
     })
 </script>
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit - its ya boy</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
