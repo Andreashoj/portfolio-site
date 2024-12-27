c8fb6defcfaf6515a56c0b1badd4259ed0b1c95e
Andreas Høj <anho@universal-robots.com>
2024-12-27 13:06:00 +0100

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,8 +7,11 @@ export default defineConfig({
 		proxy: {
 			'/api': {
 				target: 'http://localhost:5000',
-				rewrite: (path) => path.replace(/^\/api/, ''),
+				rewrite: (path) => path.replace(/^\/api/, '')
 			}
 		}
+	},
+	define: {
+		'import.meta.env.VITE_PUBLIC_API_KEY': JSON.stringify(process.env.VITE_PUBLIC_API_KEY)
 	}
 });
